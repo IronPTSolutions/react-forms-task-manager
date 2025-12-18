eb4e286fb9f7235f313ea987d2ef724f7d355751
Carlos del Prado <carlos.delpradomota@telefonica.com>
2025-12-18 22:05:54 +0100

    feat: toggle sort mode

--- src/components/tasks/task-list/task-list.jsx
+++ src/components/tasks/task-list/task-list.jsx
@@ -3,10 +3,13 @@ import TaskItem from "../task-item/task-item";
 import * as TasksService from "../../../services/tasks-service";
 import TaskForm from "../task-form/task-form";
 
+const SORT_MODE_ASC = 'asc';
+const SORT_MODE_DESC = 'desc';
+
 function TaskList({ withForm = true }) {
   const [tasks, setTasks] = useState();
   const [reload, setReload] = useState(true);
-  const [sortMode, setSortMode] = useState('asc');
+  const [sortMode, setSortMode] = useState(SORT_MODE_ASC);
 
   useEffect(() => {
     async function fetchTasks() {
@@ -26,12 +29,23 @@ function TaskList({ withForm = true }) {
 
   const handleTaskCreation = () => handleReload();
 
+  const handleSortToggle = () => {
+    if (sortMode === SORT_MODE_ASC) setSortMode(SORT_MODE_DESC);
+    else setSortMode(SORT_MODE_ASC);
+  }
+
   if (!tasks) {
     return null;
   } else {
     return (
       <>
         {withForm && (<TaskForm className="mb-3" onCreateTask={handleTaskCreation}/>)}
+
+        <div className="d-flex gap-2 justify-content-end">
+          <button type="button" className="btn btn-outline-dark btn-sm mb-2" onClick={handleSortToggle}>
+            <i className={`fa fa-sort-amount-${sortMode}`}></i>
+          </button>
+        </div>
 
         <ul className="list-group">
           {tasks.map((task) => (
